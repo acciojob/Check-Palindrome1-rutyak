c3bd8f206e672f088d685f46b91726970f422709
rutyak <132219103+rutyak@users.noreply.github.com>
2023-07-05 17:12:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 // complete the given function
-    function ReverseString(str) {
-      
-        // Check input
-        if(!str || str.length < 2 ||
-                typeof str!== 'string') {
-            return 'Not valid';
-        }
-          
-        // Take empty array revArray
-        const revArray = [];
-        const length = str.length - 1;
-          
-        // Looping from the end
-        for(let i = length; i >= 0; i--) {
-            revArray.push(str[i]);
-        }
-          
-        // Joining the array elements
-        return revArray.join('');
-    }
-      
+function ReverseString(str) {
+  // Check input
+  if (!str || str.length < 2 || typeof str !== 'string') {
+    return 'Not valid';
+  }
 
-function palindrome(str){
+  // Take empty array revArray
+  const revArray = [];
+  const length = str.length - 1;
 
+  // Looping from the end
+  for (let i = length; i >= 0; i--) {
+    revArray.push(str[i]);
+  }
+
+  // Joining the array elements
+  return revArray.join('');
+}
+
+function palindrome(str) {
   let temp = str;
   let rev = ReverseString(str);
 
-  if(temp.localeCompare(rev)){
-	  return false;
-  }
-   return true;
+  return temp === rev;
 }
-module.exports = palindrome
+
+module.exports = palindrome;
+
+const palindrome = require('./palindrome');
+
+console.log(palindrome("race a car"));
